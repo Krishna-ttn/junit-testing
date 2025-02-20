3a6d7016dfa3f3c2b80f1b6bf782fc36a5032c81
Krishna Sharma <krishna.sharma@tothenew.com>
2025-02-20 13:26:04 +0530

    Adding Tests

--- src/test/java/com/demo/service/OrderServiceTest.java
+++ src/test/java/com/demo/service/OrderServiceTest.java
@@ -4,12 +4,9 @@ import com.demo.domain.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
-
-public class EmailServiceTest {
-
+class OrderServiceTest {
     private EmailService emailService;
     private Order order;
 
@@ -18,31 +15,35 @@ public class EmailServiceTest {
         emailService = new EmailService();
         order = new Order(1, "Phone", 1000.0);
     }
+
     @Test
-    void testGetInstance_NotNull() {
-        EmailService instance1 = EmailService.getInstance();
-        //instance should not be null
+    void testGetInstance_NotNUll() {
+        OrderService instance1 = OrderService.getInstance();
         assertNotNull(instance1);
     }
-    @Test
-    void testGetInstance_Equals(){
-        EmailService instance1 = EmailService.getInstance();
-        EmailService instance2 = EmailService.getInstance();
 
+    @Test
+    void testGetInstance_Same() {
+        OrderService instance1 = OrderService.getInstance();
+        OrderService instance2 = OrderService.getInstance();
+        //instance 1 and 2 should be same
         assertEquals(instance1, instance2);
     }
+
+
     @Test
     void testSendEmail_ThrowsException() {
         Exception exception = assertThrows(RuntimeException.class, () -> emailService.sendEmail(order));
         assertEquals("An Exception Occurred", exception.getMessage());
-        //Customer should not be notified if an exception occurs
+        //if an exception occurs customer should not be notified here
         assertFalse(order.isCustomerNotified());
     }
+
     @Test
     void testSendEmail_WithCC() {
         boolean result = emailService.sendEmail(order, "cc@example.com");
         assertTrue(result);
-        //here customer should be notified
+        // here customer should be notified as succeded
         assertTrue(order.isCustomerNotified());
     }
 
