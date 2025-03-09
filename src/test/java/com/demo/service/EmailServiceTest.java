e38c10fcbf942b1dfd4095f794cd4c6f7dcb4831
Krishna Sharma <krishna.sharma@tothenew.com>
2025-03-09 21:34:32 +0530

    Small changes in EmailServiceTest

--- src/test/java/com/demo/service/EmailServiceTest.java
+++ src/test/java/com/demo/service/EmailServiceTest.java
@@ -1,28 +1,40 @@
 package com.demo.service;
 
 import com.demo.domain.Order;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class EmailServiceTest {
 
-    private final EmailService emailService = EmailService.getInstance();
+    private EmailService emailService;
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        emailService = EmailService.getInstance();
+        order = mock(Order.class);
+    }
 
     @Test
     void testSendEmail_ThrowsException() {
-        Order order = new Order(1, "Book", 100.0);
-        assertThrows(RuntimeException.class, () -> emailService.sendEmail(order));
-        assertFalse(order.isCustomerNotified());
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> emailService.sendEmail(order));
+        assertEquals("An Exception Occurred", exception.getMessage());
+        //verify that the email notification was set to false
+        verify(order).setCustomerNotified(false);
+        //verify that no notification was sent
+        verify(order, never()).setCustomerNotified(true);
     }
 
     @Test
     void testSendEmailWithCC_Success() {
-        Order order = new Order(1, "Book", 100.0);
         boolean result = emailService.sendEmail(order, "test@gmail.com");
-
         assertTrue(result);
-        assertTrue(order.isCustomerNotified());
+
+        verify(order).setCustomerNotified(true);
     }
 
     @Test
